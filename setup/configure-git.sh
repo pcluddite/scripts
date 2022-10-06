f8e7c6ab13752941d8494f26330271cc6e3fe00f
Timothy Baxendale <pcluddite@outlook.com>
2022-10-06 17:32:20 -0500

    Add editor and pull/rebase modes

--- setup/configure-git.sh
+++ setup/configure-git.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+git config --global core.editor 'vim'
+
 git config --global user.name 'Tim Baxendale'
 git config --global user.email 'pcluddite@outlook.com'
+
+git config --global pull.rebase true
+git config --global rebase.autoStash true
 
