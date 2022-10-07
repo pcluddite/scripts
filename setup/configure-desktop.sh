f8c9fb5f85dd02367dec9c4ce9c742916a09e085
Tim Baxendale <pcluddite@outlook.com>
2022-10-07 17:01:19 -0500

    Add configure-desktop.sh

---
+++ setup/configure-desktop.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+set exiterr
+
+if [[ ! -v "${COMMONDEFS}" ]]; then
+    COMMONDEFS='../common_defs.sh'
+fi
+
+if ! . "${COMMONDEFS}" STDIO; then
+    exit 1
+fi
+
+DESKTOP="${HOME}/Desktop"
+
+FILES=("home.desktop" "trash.desktop")
+ENTRIES=('[Desktop Entry]
+Encoding=UTF-8
+Name=Home
+GenericName=Personal Files
+URL[$e]=$HOME
+Icon=user-home
+Type=Link
+'
+'[Desktop Entry]
+Name=Trash
+Comment=Contains removed files
+Icon=user-trash-full
+EmptyIcon=user-trash
+Type=Link
+URL=trash:/
+OnlyShowIn=KDE;
+')
+
+for (( i=0; i < ${#FILES[@]}; ++i )); do
+    FILEPATH="${DESKTOP}/${FILES[$i]}"
+    ENTRY="${ENTRIES[$i]}"
+    YN='N'
+    if [[ -e "${FILEPATH}" ]]; then
+        YN=$(prompt_yesno --prompt="'${FILEPATH}' already exists! Replace?" --default='no')
+        if [[ "${YN}" = 'Y' ]]; then
+            YN='N'
+            if [[ -L "${FILEPATH}" ]] && unlink "${FILEPATH}"; then
+                YN='Y'
+            elif [[ -f "${FILEPATH}" ]] && rm "${FILEPATH}"; then
+                YN='Y'
+            fi
+            if [[ "${YN}" != 'Y' ]]; then
+                write_error "Skipping '${FILEPATH}'."
+            fi
+        fi
+    else
+        YN='Y'
+    fi
+    printf '%s' "${ENTRY}" > "${FILEPATH}"
+done
