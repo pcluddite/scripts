a10ca2366e76633c150be844dfd3f4992de2d3d7
Tim Baxendale <pcluddite@outlook.com>
2022-10-07 02:35:34 -0500

    Add configure-vim.sh

---
+++ setup/configure-vim.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [[ ! -v "${COMMONDEFS}" ]]; then
+    COMMONDEFS='../common_defs.sh'
+fi
+
+. "${COMMONDEFS}" STDIO
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
+
+VIMRC="$HOME/.vimrc"
+
+if [[ -f "${VIMRC}" ]]; then
+    printf '%s already exists.\n' "'${VIMRC}'"
+    if [[ $(prompt_yesno --prompt='Do you want to overwrite the existing file?' --default='no') != 'Y' ]]; then
+        exit_error 'configuration aborted'
+    fi
+fi
+
+printf \
+'set nowrap
+set expandtab
+set tabstop=4
+set visualbell
+' > "${VIMRC}"
+
+printf 'Created %s\n' "'${VIMRC}'"
+
