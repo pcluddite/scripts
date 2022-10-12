05fc826fec5e0d4a77a8025ff16e5577aa5b414e
Tim Baxendale <pcluddite@outlook.com>
2022-10-12 17:10:59 -0500

    ls_wine_drives(), path_connonical(), unlink_rm()

--- lib/fileio.sh
+++ lib/fileio.sh
@@ -5,10 +5,12 @@ if [[ "$COMMON_FILEIO" = 'Y' ]]; then
 fi
 
 is_defined() {
+    assert_arg_num -1 "$@" || return $EXIT_FAILURE
     command -v "$1" &> /dev/null
 }
 
 next_no_exist() {
+    assert_arg_num -1 "$@" || return $EXIT_FAILURE
     if [[ -f "$1" ]]; then
         local EXT="${1##*.}"
         local DIR_PATH=$(dirname "$1")
@@ -23,8 +25,46 @@ next_no_exist() {
     fi
 }
 
-cannonicalize() {
-    printf '%s' "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+path_cannonical() {
+    assert_arg_num 1 "$@" || return $EXIT_FAILURE
+    while [[ $# -gt 0 ]]; do
+        printf '%s/%s\n' "$(cd "$(dirname "$1")"; pwd)" "$(basename "$1")"
+        shift
+    done
+}
+
+unlink_rm() {
+    assert_arg_num 1 "$@" || return $EXIT_FAILURE
+    while [[ $# -gt 0 ]]; do
+        if [[ -L "$1" ]]; then
+            unlink "$1" || return $?
+        elif [[ -d "$1" ]]; then
+            rmdir "$1" || return $?
+        else [[ -f "$1" ]];
+            rm "$1" || return $?
+        fi
+        shift
+    done
+}
+
+ls_wine_drives() {
+    if [[ $# -eq 0 ]]; then
+        arr :ARGS path_cannonical "${WINEPREFIX:=${HOME}/.wine}"
+    else
+        arr :ARGS path_cannonical "$@"
+    fi
+
+    set -- "${ARGS[@]}"
+
+    [[ $# -gt 1 ]] && printf '%q:\n' "$1"
+    while [[ $# -gt 0 ]]; do
+        local WINEPREFIX="$1"
+        for FILE in "${WINEPREFIX}/dosdevices/"*':'; do
+            printf '%c:\\ %q\n' "$(basename "$FILE")" "$(readlink "$FILE")"
+        done
+        [[ $# -gt 1 ]] && printf '\n%q:\n' "$2"
+        shift
+    done
 }
 
 COMMON_FILEIO='Y'
