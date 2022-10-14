eeac5815149987d50b8580c480d6867b1fce62fe
Tim Baxendale <pcluddite@outlook.com>
2022-10-14 00:37:15 -0500

    add is_array/map and map_print

--- common_defs.sh
+++ common_defs.sh
@@ -112,7 +112,51 @@ exit_error() {
 }
 
 is_identifier() {
-    [[ "$*" =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
+    while [[ $# -gt 0 ]]; do
+        [[ "$1" =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]] || return $EXIT_FAILURE
+        shift
+    done
+    return $EXIT_SUCCESS
+}
+
+is_array() {
+    local __INVAR="$1"
+    while [[ $# -gt 0 ]]; do
+        local __DECL=$(declare -p "$__INVAR" 2> /dev/null) || return $EXIT_FAILURE
+        if [[ "${__DECL}" = 'declare -a'* ]]; then
+            shift
+            __INVAR="$1"
+        else
+            if [[ "${__DECL}" = 'declare -n'* ]]; then
+                eval __INVAR="${__DECL#*=}"
+            else
+                return $EXIT_FAILURE
+            fi
+        fi
+    done
+    return $EXIT_SUCCESS
+}
+
+is_map() {
+    local __INVAR="$1"
+    while [[ $# -gt 0 ]]; do
+        local __DECL=$(declare -p "$__INVAR" 2> /dev/null) || return $EXIT_FAILURE
+        if [[ "${__DECL}" = 'declare -A'* ]]; then
+            shift
+            __INVAR="$1"
+        else
+            if [[ "${__DECL}" = 'declare -n'* ]]; then
+                eval __INVAR="${__DECL#*=}"
+            else
+                return $EXIT_FAILURE
+            fi
+        fi
+    done
+    return $EXIT_SUCCESS
+}
+
+is_root() {
+    [[ $(id -u) = 0 ]];
 }
 
 assert_arg_num() {
@@ -147,10 +191,6 @@ assert_identifier() {
     is_identifier "$@" || return_error "'$*' is not a valid identifier"
 }
 
-is_root() {
-    [[ $(id -u) = 0 ]];
-}
-
 assert_root() {
     assert_arg_num 0 "$@" || return $EXIT_FAILURE
     if ! is_root; then
@@ -158,19 +198,43 @@ assert_root() {
     fi
 }
 
-arr_print() {
-    assert_arg_num 1 "$@" || return $EXIT_FAILURE
-    
-    printf '%q' "$1"
-    shift
-
-    while [[ $# -gt 0 ]]; do
-        printf '\n%q' "$1"
-        shift
-    done
+assert_array() {
+    is_array "$@" || return_error "'$*' is not an array"
 }
 
-arr() {
+assert_map() {
+    is_map "$@" || return_error "'$*' is not an associative array"
+}
+
+map_print() {
+    assert_arg_num -1 "$@" || return $EXIT_FAILURE
+
+    declare -n _MAPVAR="${1#:}" || return $?
+    assert_map _MAPVAR || return $EXIT_FAILURE
+
+    set -- "${!_MAPVAR[@]}"
+
+    printf '('
+    while [[ $# -gt 0 ]]; do
+        printf ' [%s]=%q ' "$1" "${_MAPVAR[$1]}"
+        shift
+    done
+    printf ')'
+}
+
+arr_print() {
+    assert_arg_num -1 "$@" || return $EXIT_FAILURE
+    declare -n __INVAR="${1#:}" || return $?
+    assert_arr __INVAR || return $EXIT_FAILURE
+
+    set -- "${!__INVAR[@]}"
+
+    printf '('
+    [[ $# -gt 0 ]] && printf ' %q ' "$@"
+    printf ')'
+}
+
+read_arr() {
     assert_arg_num 1 "$@" || return $EXIT_FAILURE
 
     local __OUTVAR="$1"
@@ -190,19 +254,19 @@ arr() {
     local __ARGS=()
 
     if [[ "$1" =~ -[vtf]+.* ]]; then
-        local ARG="${1:2}"
+        local __ARG="${1:2}"
         __OPTION="${1:1:1}"
         shift
-        if [[ "${ARG}" = '' ]]; then
+        if [[ "${__ARG}" = '' ]]; then
             if [[ $# -eq 0 ]]; then
                 write_error "no value specified for '-${__OPTION}'"
                 return $EXIT_FAILURE
             fi
         else
-            if [[ "${ARG}" = '='* ]] || [[ "${ARG}" = ':'* ]]; then
-                ARG="${ARG:1}"
+            if [[ "${__ARG}" = '='* ]] || [[ "${__ARG}" = ':'* ]]; then
+                __ARG="${__ARG:1}"
             fi
-            __ARGS+=("${ARG}")
+            ____ARGS+=("${__ARG}")
         fi
     elif [[ "$1" = '-c' ]]; then
         __OPTION='c'
