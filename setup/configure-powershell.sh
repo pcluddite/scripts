c5ede7e11083a97fc7d0769bc7f18d6625c919f0
Tim Baxendale <pcluddite@outlook.com>
2022-10-15 23:40:10 -0500

    Add configure-powershell.sh

---
+++ setup/configure-powershell.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ ! -v "${COMMONDEFS}" ]]; then
+    COMMONDEFS="$(dirname "$0")/../common_defs.sh"
+fi
+
+if ! . "${COMMONDEFS}" ERREXIT; then
+    return $EXIT_FAILURE
+fi
+
+assert_root
+
+if [[ "$1" = '-u' || "$1" = '--uninstall' ]]; then
+    if dnf remove powershell; then
+        rm -v /etc/yum.repos.d/microsoft.repo
+    fi
+else
+    rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    curl https://packages.microsoft.com/config/rhel/7/prod.repo | tee /etc/yum.repos.d/microsoft.repo
+    dnf makecache
+    dnf install powershell
+fi
--- tunnel.sh
+++ tunnel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 TIMS_NEWDESKTOP=192.168.0.37
-tmux new -ds tunnel ssh -L 5500:$TIMS_NEWDESKTOP:3389 minecraft.timbaxendale.com -p9022
+tmux new -ds tunnel ssh -L 5500:$TIMS_NEWDESKTOP:5900 minecraft.timbaxendale.com -p9022
