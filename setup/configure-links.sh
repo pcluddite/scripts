5aa52b71b22c29737f418ae886e221fe4b953897
Tim Baxendale <pcluddite@outlook.com>
2022-10-16 21:55:55 -0500

    Update configure-links.sh

--- setup/configure-links.sh
+++ setup/configure-links.sh
@@ -4,62 +4,51 @@ if [[ ! -v COMMONDEFS ]]; then
     COMMONDEFS="$(dirname "$0")/../common_defs.sh"
 fi
 
-if ! . "${COMMONDEFS}" STDIO ERREXIT; then
+if ! . "${COMMONDEFS}" FILEIO STDIO ERREXIT; then
     exit 1
 fi
 
-ONEDRIVE="${HOME}/OneDrive"
-HOME_FOLDERS=(Documents Pictures Music Videos 'Disc Images' Games)
-
-for (( i=0; i < ${#HOME_FOLDERS[@]}; ++i )); do
-    FOLDER="${HOME_FOLDERS[$i]}"
-    HOME_DIR="${HOME}/${FOLDER}"
-    ONEDRIVE_DIR="${ONEDRIVE}/${FOLDER}"
-    YN='N'
-    if [[ ! -d "${ONEDRIVE_DIR}" ]]; then
-        YN='N'
-        write_error "'${ONEDRIVE_DIR}' does not exist. It cannot be linked to '${HOME_DIR}'"
-    elif [[ -d "${HOME_DIR}" ]]; then
-        YN=$(prompt_yesno --prompt="'${HOME_DIR}' already exists! Replace?" --default='no')
-        if [[ "${YN}" = 'Y' ]]; then
-            YN='N'
-            if [[ -L "${HOME_DIR}" ]] && unlink "${HOME_DIR}"; then
-                YN='Y'
-            elif [[ -d "${HOME_DIR}" ]] && rmdir "${HOME_DIR}"; then
-                YN='Y'
+link_home() {
+    while [[ $# -gt 0 ]]; do
+        local LINK="${HOME}/${1%%=*}"
+        local TARGET="${1#*=}"
+        local YN='N'
+        if [[ ! -e "${TARGET}" ]]; then
+            write_error "'${TARGET}' does not exist. It cannot be linked to '${LINK}'"
+        elif [[ -e "${LINK}" || -L "${LINK}" ]]; then
+            YN=$(prompt_yesno --prompt="'$(pretty_path "${LINK}")' already exists! Replace with '$(pretty_path "${TARGET}")'?" --default='no')
+            if [[ "${YN}" = 'Y' ]] && ! unlink_rm "${LINK}"; then
+                YN='N'
+                write_error "Skipping '$(pretty_path "${TARGET}")' because '$(pretty_path "${LINK}")' could not be removed."
             fi
-            if [[ "${YN}" != 'Y' ]]; then
-                write_error "Skipping '${ONEDRIVE_DIR}' because '${HOME_DIR}' could not be removed."
-            fi
+        else
+            YN='Y'
         fi
-    else
-        YN='Y'
-    fi
-    if [[ "${YN}" = 'Y' ]]; then
-        ln -s "${ONEDRIVE_DIR}" "${HOME_DIR}"
-    fi
-done
-
-HOME_FILES=('.bash_history' '.bash_profile')
-HOME_LINKS=(/dev/null "${COMMON_DIR}/bash_profile")
-
-for (( i=0; i < ${#HOME_FILES[@]}; ++i )); do
-    FILE="${HOME}/${HOME_FILES[$i]}"
-    LINK="${HOME_LINKS[$i]}"
-    YN='N'
-    if [[ -e "${FILE}" ]]; then
-        YN=$(prompt_yesno --prompt="'${FILE}' already exists! Replace with '${LINK}'?" --default='no')
         if [[ "${YN}" = 'Y' ]]; then
-            if [[ -L "${FILE}" ]]; then
-                unlink "${FILE}"
-            else
-                rm "${FILE}"
-            fi
+            ln -s "${TARGET}" "${LINK}"
         fi
-    else
-        YN='Y'
-    fi
-    if [[ "${YN}" = 'Y' ]]; then
-        ln -s "${LINK}" "${FILE}"
-    fi
-done
+        shift
+    done
+}
+
+link_onedrive() {
+    local ONEDRIVE="${HOME}/OneDrive"
+    local ARGS=()
+    while [[ $# -gt 0 ]]; do
+        ARGS+=("$1=${ONEDRIVE}/$1")
+        shift
+    done
+    link_home "${ARGS[@]}"
+}
+
+link_home \
+    '.bash_history'='/dev/null' \
+    '.bash_profile'="${COMMON_DIR}/bash_profile"
+
+link_onedrive \
+    Documents \
+    Pictures \
+    Music \
+    Videos \
+    'Disc Images' \
+    Games
