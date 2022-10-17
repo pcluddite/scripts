95821f92e7f3cd7b89ccaf769ffa221601a59c00
Tim Baxendale <pcluddite@outlook.com>
2022-10-16 20:50:20 -0500

    Add setup/configure-wine.sh

---
+++ setup/configure-wine.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ ! -v "${COMMONDEFS}" ]]; then
+    COMMONDEFS="$(dirname "$0")/../common_defs.sh"
+fi
+
+if ! . "${COMMONDEFS}" FILEIO ERREXIT; then
+    return $EXIT_FAILURE
+fi
+
+if ! is_defined 'wine' || ! is_defined cabextract; then
+    assert_root
+    dnf install wine cabextract
+fi
+
+mkcd "${BIN_DIR}"
+wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
