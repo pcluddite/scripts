5ca1036624aedb89808786ff4c8d3651445987a9
Tim Baxendale <pcluddite@outlook.com>
2022-10-16 20:46:23 -0500

    Update configure-git.sh

--- setup/configure-git.sh
+++ setup/configure-git.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
-git config --global core.editor 'vim'
+if [[ ! -v COMMONDEFS ]]; then
+    COMMONDEFS="$(dirname "$0")/../common_defs.sh"
+fi
 
-git config --global user.name 'Tim Baxendale'
-git config --global user.email 'pcluddite@outlook.com'
+if ! . "${COMMONDEFS}" ARRAY ERREXIT; then
+    exit 1
+fi
 
-git config --global pull.rebase true
-git config --global rebase.autoStash true
+declare -A CONFIGS=(
+    [core.editor]='vim'
+    [user.name]='Tim Baxendale'
+    [user.email]='pcluddite@outlook.com'
+    [pull.rebase]='true'
+    [rebase.autoStash]='true'
+)
 
+for CFG_KEY in "${!CONFIGS[@]}"; do
+    CFG_VALUE="${CONFIGS[${CFG_KEY}]}"
+    printf '[%s] = %s: ' "${CFG_KEY}" "${CFG_VALUE}"
+    if git config --global -- "${CFG_KEY}" "${CFG_VALUE}"; then
+        printf '%s\n' 'OK'
+    else
+        printf '%s\n' 'FAILED'
+    fi
+done
