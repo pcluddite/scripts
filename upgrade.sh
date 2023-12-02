d8f7d82af31341d77d1492fe27021d32564a48f2
Tim Baxendale <pcluddite@outlook.com>
2023-12-01 23:09:47 -0600

    Add upgrade.sh

---
+++ upgrade.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ ! -v COMMONDEFS ]]; then
+    COMMONDEFS="$(dirname "$0")/common_defs.sh"
+fi
+
+if ! . "${COMMONDEFS}"; then
+    exit 1
+fi
+
+assert_root
+
+dnf check-update --refresh
+EXIT_STATUS=$?
+if [[ $EXIT_STATUS -eq 100 ]]; then
+    if dnf upgrade; then
+        echo 'System will reboot in 1 minute. Re-run this script after start-up to continue upgrade.'
+        shutdown -r +1 'System scheduled to reboot'
+        exit 0
+    fi
+elif [[ $EXIT_STATUS -ne 0 ]]; then
+    echo 'Upgrade could not complete'
+    exit 1
+fi
+
+. /etc/os-release
+
+NEXT_VERSION=$(( $VERSION_ID + 1 ))
+echo "Preparing to upgrade to ${NAME} ${NEXT_VERSION}"
+
+if dnf install dnf-plugin-system-upgrade; then
+    if dnf system-upgrade download --releasever="${NEXT_VERSION}"; then
+        dnf system-upgrade reboot
+    fi
+fi
+
