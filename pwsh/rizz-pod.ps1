cc0a048ee64e7f581cd92b750a8686b94ea09d24
Tim Baxendale <pcluddite@outlook.com>
2023-12-23 00:16:32 -0600

    Add HTML parsing to ./rizz-pod.ps1

--- pwsh/rizz-pod.ps1
+++ pwsh/rizz-pod.ps1
@@ -5,86 +5,97 @@ param(
 )
 
 $ErrorActionPreference = 'Stop'
-
 $INVALID_CHARS = "[{0}]" -f [Regex]::Escape([Path]::GetInvalidFileNameChars() -join '')
 
-function Get-FileName {
+# Install the module on demand (https://stackoverflow.com/a/60658511/4367864)
+if (-not (Get-Module -ErrorAction Ignore -ListAvailable PSParseHTML)) {
+    Write-Verbose "Installing PSParseHTML module for the current user..."
+    Install-Module -Scope CurrentUser PSParseHTML -ErrorAction Stop
+}
+  
+function Get-Filename {
     param (    
         [Parameter(Mandatory)]
         [string]$Title,
         [Parameter(Mandatory)]
-        [string]$Url
+        [string]$Uri
     )
-    $nStart = $Url.LastIndexOf('/')
-    $nEnd = $Url.LastIndexOf('?')
+    $nStart = $Uri.LastIndexOf('/')
+    $nEnd = $Uri.LastIndexOf('?')
     if ($nEnd -le $nStart) {
-        $nEnd = $Url.Length
+        $nEnd = $Uri.Length
     }
-    $ext = [Path]::GetExtension($Url.Substring($nStart + 1, $nEnd - $nStart - 1))
+    $ext = [Path]::GetExtension($Uri.Substring($nStart + 1, $nEnd - $nStart - 1))
     return "${Title}${ext}" -replace $INVALID_CHARS, '%'
-}
-function Out-Unescape() {
-    param(
-        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
-        [string[]]$InputString
-    )
-    begin {
-        $sb = New-Object -TypeName System.Text.StringBuilder
-    }
-    process {
-        $InputString | % {
-            if ($_ -eq '') {
-                return $_
-            }
-            $sb=$sb.Clear()
-            $i = 0
-            for(; $i -lt $_.Length - 1; ++$i) {
-                $c=$_[$i]
-                if ($c -eq '\') {
-                    $c=$_[++$i]
-                    if ($c -eq 'n') {
-                        $c=[char]"`n"
-                    } elseif ($c -eq 'r') {
-                        $c=[char]"`r"
-                    } elseif ($c -eq 't') {
-                        $c=[char]"`t"
-                    } elseif ($c -eq '0') {
-                        $c=[char]"`0"
-                    } elseif ($c -eq 'b') {
-                        $c=[char]"`b"
-                    }
-                }
-                $sb=$sb.Append($c)
-            }
-            $sb=$sb.Append($_[$i])
-            return $sb.ToString()
-        }
-    }
 }
 
 function Get-Mp3Uri() {
     param(
-        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
-        [string[]]$Url
+        [Parameter(Mandatory,Position=0)]
+        [Alias('Uri')]
+        [string[]]$EpisodeUrl
     )
     process {
-        $Url | % {
-            $EpResponse=(Invoke-WebRequest -Uri $_)
-            $html=$EpResponse.Content
-            $Field = '"mp3":"'
-            $startIdx=$html.IndexOf($Field) + $Field.Length
-            $endIdx=$html.IndexOf('"', $startIdx)
-            $html.Substring($startIdx, $endIdx - $startIdx) | Out-Unescape
+        $EpisodeUrl | % {
+            $Response=(Invoke-WebRequest -Uri $_)
+            
+            $HtmlNode=($Response.Content | ConvertFrom-HTML)
+
+            $Script=($HtmlNode.SelectNodes("//div[@class='entry-content']/script") | % { $_.InnerText.Trim() } | where { $_ -ne '' } | select -First 1)
+
+            $endIdx=$Script.LastIndexOf(';')
+            $startIdx=$Script.IndexOf('=') + 1
+            $Json=($Script.Substring($startIdx, $endIdx - $startIdx).Trim() | ConvertFrom-Json)
+            return $Json.episode.media.mp3
         }
     }
 }
 
-$PageUrl = "https://www.1057thepoint.com/podcasts/the-rizzuto-show/?episode_page=${Page}"
-$Response = Invoke-WebRequest -Uri $PageUrl
-$Response.Links | where { $_.href -like 'https://www.1057thepoint.com/episode/*' } | % { 
-    $Title=$_.outerHTML.Substring($_.outerHTML.IndexOf('>') + 1)
-    $Title=$Title.Remove($Title.LastIndexOf('<'))
-    $Mp3Uri = $($_.href | Get-Mp3Uri)
-    Write-Host "${Title}: ${Mp3Uri}"
-    Invoke-WebRequest -Uri $Mp3Uri -OutFile (Get-FileName -Title $Title -Url $Mp3Uri)
+function Get-Articles {
+    param(
+        [Parameter(Mandatory,Position=0)]
+        [int]$Page
+    )
+
+    $PageUrl="https://www.1057thepoint.com/podcasts/the-rizzuto-show/?episode_page=${Page}"
+
+    $Response=Invoke-WebRequest -Uri $PageUrl
+    $HtmlNode=($Response.Content | ConvertFrom-HTML)
+
+    # select article nodes for latest episodes
+    $Articles=$HtmlNode.SelectNodes("//div[@class='latest-episodes']/article")
+
+    $Articles | % { $_.InnerHtml | ConvertFrom-HTML } | % {     
+        # select link node to each article from post-title class
+        $LinkNode=$_.SelectSingleNode("//*[@class='post-title']/a")
+        return [PSCustomObject]@{
+            # decode innerText to remove &###;
+            Title=[Net.WebUtility]::HtmlDecode($LinkNode.InnerText)
+
+            # get url from href attribute
+            Url=$LinkNode.Attributes['href'].Value
+            
+            # select published date from time node
+            PublishDate=[DateTime]$_.SelectSingleNode("//time").InnerText
+        }
+    }
 }
+
+function Get-Episode {
+    param(
+        [Parameter(Mandatory,Position=0)]
+        [string]$Title,
+        [Parameter(Mandatory,Position=1)]
+        [string]$Url,
+        [Parameter(Mandatory,Position=2)]
+        [DateTime]$PublishDate,
+        [Parameter(Mandatory,Position=3)]
+        [string]$OutPath
+    )
+    $Uri=(Get-Mp3Uri $Url)
+    $Filename=Get-Filename -Title $Title -Uri $Uri
+    $FullPath=Join-Path $OutPath $Filename
+    Invoke-WebRequest -Uri $Uri -OutFile $FullPath
+}
+
+Get-Articles -Page $Page
