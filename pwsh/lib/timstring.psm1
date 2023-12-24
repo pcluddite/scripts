103278a8525473c505ee3d0e59568a77e8747744
Tim Baxendale <pcluddite@outlook.com>
2023-12-24 14:30:34 -0600

    Add Out-Alpha and Out-Alphanumeric

--- pwsh/lib/timstring.psm1
+++ pwsh/lib/timstring.psm1
@@ -13,3 +13,44 @@ function Out-Truncate {
         return $str
     }
 }
+
+function Out-Alpha {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [psobject]$InputObject
+    )
+    begin {
+        $sb=[Text.StringBuilder]::new()
+    }
+    process {
+        $str = ($InputObject | Out-String -NoNewline)
+        foreach($c in [char[]]$str) {
+            if ([char]::IsLetter($c) -or [char]::IsWhiteSpace($c)) {
+                $sb=$sb.Append($c)
+            }
+        }
+        $sb.ToString()
+        $sb=$sb.Clear()
+    }
+}
+
+
+function Out-Alphanumeric {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [psobject]$InputObject
+    )
+    begin {
+        $sb=[Text.StringBuilder]::new()
+    }
+    process {
+        $str = ($InputObject | Out-String -NoNewline)
+        foreach($c in [char[]]$str) {
+            if ([char]::IsLetterOrDigit(($c) -or [char]::IsWhiteSpace($c)) {
+                $sb=$sb.Append($c)
+            }
+        }
+        $sb.ToString()
+        $sb=$sb.Clear()
+    }
+}
