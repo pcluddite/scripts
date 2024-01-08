c0c60452f9e81ea96232bb47dbb17e5221da3118
Tim Baxendale <pcluddite@outlook.com>
2024-01-08 13:59:06 -0600

    Update ms repo

--- setup/install-powershell.sh
+++ setup/install-powershell.sh
@@ -16,7 +16,7 @@ if [[ "$1" = '-u' || "$1" = '--uninstall' ]]; then
     fi
 else
     rpm --import https://packages.microsoft.com/keys/microsoft.asc
-    curl https://packages.microsoft.com/config/rhel/7/prod.repo | tee /etc/yum.repos.d/microsoft.repo
+    curl https://packages.microsoft.com/config/rhel/8/prod.repo | tee /etc/yum.repos.d/microsoft.repo
     dnf makecache
     dnf install powershell
 fi
