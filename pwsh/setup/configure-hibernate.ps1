ead41ac13f7c04e3bdec1140cf55ef58ea5eee1c
Tim Baxendale <pcluddite@outlook.com>
2024-01-17 23:44:42 -0500

    Add overrides and systemd services

--- pwsh/setup/configure-hibernate.ps1
+++ pwsh/setup/configure-hibernate.ps1
@@ -197,7 +197,83 @@ function Update-Grub {
     }
 }
 
-#Build-SwapFile -SwapVolume $SwapVolume -SwapFile $SwapFile -InformationAction Continue
+function Add-HibernateService {
+    param(
+        [Parameter(Mandatory)]
+        [string]$SwapPath
+    )
+"[Unit]
+Description=Enable swap file and disable zram before hibernate
+Before=systemd-hibernate.service
+
+[Service]
+User=root
+Type=oneshot
+ExecStart=/bin/bash -c `"/usr/sbin/swapon ${SwapPath} && /usr/sbin/swapoff /dev/zram0`"
+
+[Install]
+WantedBy=systemd-hibernate.service" > '/etc/systemd/system/hibernate-preparation.service'
+
+    Write-Information 'Enabling hibernate-preparation.service'
+    systemctl enable hibernate-preparation.service
+
+"[Unit]
+Description=Disable swap after resuming from hibernation
+After=hibernate.target
+
+[Service]
+User=root
+Type=oneshot
+ExecStart=/usr/sbin/swapoff ${SwapPath}
+
+[Install]
+WantedBy=hibernate.target" > '/etc/systemd/system/hibernate-resume.service'
+
+    Write-Information 'Enabling hibernate-resume.service'
+    systemctl enable hibernate-resume.service
+}
+
+function Disable-SystemdCheck {
+    [CmdletBinding()]
+    param()
+    $LogindPath='/etc/systemd/system/systemd-logind.service.d'
+
+    if (Test-Path $LogindPath) {
+        Write-Information "${LogindPath} already exists"
+    } else {
+        New-Item -Path $LogindPath -ItemType Directory | Out-Null
+        Write-Information "Created ${LogindPath}"
+    }
+
+"[Service]
+Environment=SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK=1
+" > "${LogindPath}/override.conf" | Out-Null
+
+    Write-Information "Created ${LogindPath}/override.conf"
+
+    $HibernatedPath='/etc/systemd/system/systemd-hibernate.service.d'
+
+    if (Test-Path $HibernatedPath) {
+        Write-Information "${HibernatedPath} already exists"
+    } else {
+        New-Item -Path $HibernatedPath -ItemType Directory | Out-Null
+        Write-Information "Created ${HibernatedPath}"
+    }
+
+"[Service]
+Environment=SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK=1
+" > "${HibernatedPath}/override.conf" | Out-Null
+
+    Write-Information "Created ${HibernatedPath}/override.conf"
+}
+
+Build-SwapFile -SwapVolume $SwapVolume -SwapFile $SwapFile -InformationAction Continue
 Add-ResumeModule '/etc/dracut.conf.d/resume.conf' -InformationAction Continue
 
-Update-Grub -SwapPath "${SwapVolume}/${SwapFile}" -InformationAction Continue
+$SwapPath="${SwapVolume}/${SwapFile}"
+
+Update-Grub -SwapPath $SwapPath -InformationAction Continue
+Add-HibernateService -SwapPath $SwapPath -InformationAction Continue
+Disable-SystemdCheck -InformationAction Continue
+
+Write-Information 'Hibernation enabled. Restart system for changes to take effect.'
