3e88ef2788d5b66d55594ee99843b655480e85d1
Tim Baxendale <pcluddite@outlook.com>
2024-01-22 22:38:51 -0600

    Add pwsh/scan-hubbard.ps1

---
+++ pwsh/scan-hubbard.ps1
@@ -0,0 +1,24 @@
+
+$Start=999999999
+$Count=9999999999
+
+for($i = $Start; $i -lt $Count; ++$i) { 
+    $Name="HUBB$($i.ToString().PadLeft(10, '0')).mp3"
+
+    $Percent=($i - $Start) / ($Count - $Start) * 100
+
+    Write-Progress `
+        -Activity 'Scanning and downloading mp3s' `
+        -Status "Scanning $($Percent.ToString('0.000'))%" `
+        -PercentComplete $Percent
+
+    $Uri="https://traffic.megaphone.fm/${Name}"
+    try {
+        Invoke-WebRequest -Uri $Uri -OutFile 'D:\Hubbard' -AllowInsecureRedirect
+    } catch {
+        $Status=$_.Exception.Response.StatusCode
+        if ($Status -ne 404) {
+            Write-Error $_
+        }
+    }
+}
