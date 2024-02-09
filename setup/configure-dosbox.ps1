659f75419fdf917af5052c740fd60ec6507ceedc
Tim Baxendale <pcluddite@outlook.com>
2024-02-08 23:01:41 -0600

    Update ./configure-dosbox.ps1

--- setup/configure-dosbox.ps1
+++ setup/configure-dosbox.ps1
@@ -1,60 +1,60 @@
+[CmdletBinding(SupportsShouldProcess,ConfirmImpact='High')]
 param(
-    [string]$ConfigPath,
-    [string]$OneDrive
+    [string]$ConfigPath = "${HOME}/.dosbox/dosbox-0.74-3.conf",
+    [string]$OneDrive = "${HOME}/OneDrive"
 )
+trap {
+    $PSCmdlet.ThrowTerminatingError($_)
+}
 
 $ErrorActionPreference = 'Stop'
 
-. "${PSScriptRoot}/modules.ps1" -Name @('files')
+. (Join-Path $PSScriptRoot '../modules.ps1') -Name @('files')
 
-if [[ ! -v COMMONDEFS ]]; then
-    COMMONDEFS="$(dirname "$0")/../common_defs.sh"
-fi
+$ConfigPath=[IO.Path]::GetFullPath($ConfigPath)
+$OneDrive=[IO.Path]::GetFullPath($OneDrive)
+$DiscPath=Join-Path '%ONEDRIVE%' 'Disc Images'
+$DOSBoxPath=Join-Path '%ONEDRIVE%' 'Program Files/DOSBox-X'
 
-if ! . "${COMMONDEFS}" ARRAY STDIO FILEIO ERREXIT; then
-    exit 1
-fi
+$Drives=@{
+    'C'=Join-Path '%DOSBOX%' 'win'
+    'D'=Join-Path '%ONEDRIVE%' 'My Apps/Desktop/MS-DOS'
+    'G'=Join-Path '%ONEDRIVE%' 'Games'
+    'I'=Join-Path '%DOSBOX%' 'install'
+}
 
-CFG_FILE="${HOME}/.dosbox/dosbox-0.74-3.conf"
-ONEDRIVE="${HOME}/OneDrive"
+if (-not (Test-Path $ConfigPath)) {
+    throw [IO.FileNotFoundException]"Could not find '${ConfigPath}'"
+}
 
-declare -A DRIVES=(
-    [C]="${ONEDRIVE}/Program Files/DOSBox-X/win"
-    [D]="${ONEDRIVE}/My Apps/Desktop/MS-DOS"
-    [G]="${ONEDRIVE}/Games"
-)
-
-if [[ ! -e "${CFG_FILE}" ]]; then
-    exit_error "'${CFG_FILE}' does not exist"
-fi
-
-if [[ $(prompt_yesno --prompt='Replace [autoexec] section?' --default='no') != 'Y' ]]; then
-    exit_error 'configuration aborted'
-fi
-
-TMP_FILE=$(file_next_index "${CFG_FILE}.tmp")
-
-tr -d '\r' < "${CFG_FILE}" | while read -r LINE; do
-    if [[ "${LINE}" = '[autoexec]' ]]; then
-        printf '%s\n' '[autoexec]'
-        printf '%s\n' '# Lines in this section will be run at startup.'
-
-        for LETTER in $(arr_sort -- "${!DRIVES[@]}"); do
-            printf 'MOUNT %c "%s"\n' "${LETTER}" "${DRIVES[${LETTER}]}"
-        done
-
-        printf '%s\n' 'SET TEMP=C:\WINDOWS\TEMP'
-        printf '%s\n' 'PATH %PATH%;C:\CMD;C:\QB'
-        printf '%s\n' 'CALL C:\VBDOS\BIN\NEW-VARS.BAT'
-        printf '%s\n' 'CALL C:\MSVC\BIN\MSVCVARS.BAT'
-        printf '%s\n' 'C:'
-        printf '%s\n' 'cls'
-        break
-    else
-        printf '%s\n' "${LINE}"
-    fi
-done > "${TMP_FILE}"
-
-if unix2dos -q -n "${TMP_FILE}" "${CFG_FILE}"; then
-    unlink_rm "${TMP_FILE}"
-fi
+if ($PSCmdlet.ShouldProcess('Replace [autoexec]', 'Replace [autoexec] section?', $MyInvocation.MyCommand)) {
+    #$TempFile=[IO.Path]::GetTempFileName()
+    $Lines=Get-Content $ConfigPath
+    $(foreach($Line in $Lines) {
+        Write-Output $Line
+        if ($Line -eq '[autoexec]') {
+            Write-Output '# Lines in this section will be run at startup.'
+            Write-Output '# You can put your MOUNT lines here.'
+            Write-Output '@ECHO OFF'
+            Write-Output "SET ONEDRIVE=${OneDrive}"
+            Write-Output "SET DOSBOX=${DOSBOxPath}"
+            Write-Output "SET DISCS=${DiscPath}"
+            $Drives.Keys | Sort-Object | % {
+                Write-Output ('MOUNT {0} "{1}"' -f $_, $Drives[$_])
+            }
+            Write-Output 'SET TEMP=C:\WINDOWS\TEMP'
+            Write-Output 'PATH %PATH%;C:\CMD;C:\QB'
+            Write-Output 'CALL C:\VBDOS\BIN\NEW-VARS.BAT'
+            Write-Output 'CALL C:\MSVC\BIN\MSVCVARS.BAT'
+            Write-Output 'C:'
+            Write-Output 'CLS'
+            Write-Output 'ECHO.'
+            Write-Output 'VER'
+            Write-Output 'ECHO.'
+            Write-Output "ECHO Type 'win' and press enter to start Windows 3.11"
+            Write-Output 'ECHO.'
+            break
+        }
+    }) > $ConfigPath
+    #Move-Item -Force -LiteralPath $TempFile -Destination $ConfigPath -Confirm:$false -WhatIf:$false
+}
