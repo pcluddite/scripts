41371f6e160eb8e341e2a980825ce3f2bf36f05d
Tim Baxendale <pcluddite@outlook.com>
2024-02-22 23:02:12 -0600

    Write-Progress for dupes

--- find-duplicates.ps1
+++ find-duplicates.ps1
@@ -5,7 +5,7 @@ using namespace System.IO
 [SuppressMessageAttribute('PSReviewUnusedParameter','Filter',Justification='false positive')]
 [SuppressMessageAttribute('PSReviewUnusedParameter','Recurse',Justification='false positive')]
 param(
-    [Parameter(Mandatory,Position=0,ValueFromPipeline)]
+    [Parameter(Mandatory,Position=0)]
     [string[]]$Path,
     [Parameter(ValueFromRemainingArguments,Position=1)]
     [string[]]$Filter,
@@ -14,50 +14,64 @@ param(
     [Parameter()]
     [string]$Algorithm = 'MD5'
 )
-begin {
-    $MODULE_PATH=Join-Path $PSScriptRoot 'modules.ps1'
-    . $MODULE_PATH -Name @('files')
+$MODULE_PATH=Join-Path $PSScriptRoot 'modules.ps1'
+. $MODULE_PATH -Name @('files')
 
-    $FileMap=@{}
+$FileMap=@{}
 
-    function Find-Duplicate {
-        param(
-            [Parameter(Mandatory,Position=0)]
-            [string]$Path,
-            [Parameter(Position=1)]
-            [string]$Filter,
-            [Parameter()]
-            [string]$Algorithm='MD5',
-            [Parameter()]
-            [switch]$Recurse
-        )
-        Get-ChildItem -LiteralPath:$Path -Filter:$Filter -Recurse:$Recurse -File | % {
-            $Hash=(Get-FileHash -LiteralPath $_.FullName -Algorithm $Algorithm).Hash
-            if ($FileMap[$Hash]) {
-                $FileMap[$Hash]+=$_
-            } else {
-                $FileMap[$Hash]=@($_)
-            }
-        }
-    }
-}
-process {
-    $Path | % {
-        $LiteralPath=[Path]::GetFullPath($_)
-        if ($Filter) {
-            $Filter | % {
-                Find-Duplicate -Path $LiteralPath -Filter $_ -Recurse:$Recurse -Algorithm $Algorithm
-            }
+function Find-Duplicate {
+    param(
+        [Parameter(Mandatory,Position=0)]
+        [string]$Path,
+        [Parameter(Position=1)]
+        [string]$Filter,
+        [Parameter()]
+        [string]$Algorithm='MD5',
+        [Parameter()]
+        [switch]$Recurse
+    )
+    $AllFiles=@(Get-ChildItem -LiteralPath:$Path -Filter:$Filter -Recurse:$Recurse -File)
+    $i=0
+    $AllFiles | % {
+        $Hash=(Get-FileHash -LiteralPath $_.FullName -Algorithm $Algorithm).Hash
+        $FileSet=$FileMap[$Hash]
+        if ($FileSet) {
+            $FileSet[$_.ResolvedTarget]=$true
         } else {
-            Find-Duplicate -Path $LiteralPath -Recurse:$Recurse -Algorithm $Algorithm
+            $FileSet=@{"$($_.ResolvedTarget)"=$true}
         }
+        $FileMap[$Hash]=$FileSet
+        Write-Progress -Activity 'Duplicate search status' `
+            -Status "Searching '${_}'" `
+            -PercentComplete ([double]$i++ / $AllFiles.Length * 100)
     }
 }
-end {
-    $FileMap.Keys | % {
-        [PSCustomObject]@{
-            Hash = $_
-            Paths = ($FileMap[$_] | select -Unique | sort -Property FullName)
+
+$i=0
+
+$Path | % {
+    trap {
+        $PSCmdlet.WriteError($_)
+    }
+
+    $Directory=(Get-Item $_ -ErrorAction SilentlyContinue)
+    if (-not $Directory) {
+        throw [DirectoryNotFoundException]"'${_}' does not exist"
+    } elseif ($Directory -isnot [DirectoryInfo]) {
+        throw [DirectoryNotFoundException]"'${_}' is not a directory"
+    } elseif ($Filter) {
+        $Filter | % {
+            Find-Duplicate -Path $Directory -Filter $_ -Recurse:$Recurse -Algorithm $Algorithm
         }
-    } | where { $_.Paths.Length -gt 1 }
+    } else {
+        Find-Duplicate -Path $Directory -Recurse:$Recurse -Algorithm $Algorithm
+    }
+    ++$i
 }
+
+$FileMap.Keys | % {
+    [PSCustomObject]@{
+        Hash = $_
+        Paths = @($FileMap[$_].Keys | sort -Property FullName)
+    }
+} | where { $_.Paths.Length -gt 1 }
