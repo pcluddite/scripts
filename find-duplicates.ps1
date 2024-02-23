38be77b4120b27275482515f14212dd576180d8e
Tim Baxendale <pcluddite@outlook.com>
2024-02-22 22:36:12 -0600

    Move logic into function

--- find-duplicates.ps1
+++ find-duplicates.ps1
@@ -1,10 +1,17 @@
+using namespace System.Diagnostics.CodeAnalysis;
 using namespace System.IO
+
+[SuppressMessageAttribute('PSReviewUnusedParameter','Algorithm',Justification='false positive')]
+[SuppressMessageAttribute('PSReviewUnusedParameter','Filter',Justification='false positive')]
+[SuppressMessageAttribute('PSReviewUnusedParameter','Recurse',Justification='false positive')]
 param(
     [Parameter(Mandatory,Position=0,ValueFromPipeline)]
     [string[]]$Path,
     [Parameter(ValueFromRemainingArguments,Position=1)]
     [string[]]$Filter,
+    [Parameter()]
     [switch]$Recurse,
+    [Parameter()]
     [string]$Algorithm = 'MD5'
 )
 begin {
@@ -12,20 +19,37 @@ begin {
     . $MODULE_PATH -Name @('files')
 
     $FileMap=@{}
+
+    function Find-Duplicate {
+        param(
+            [Parameter(Mandatory,Position=0)]
+            [string]$Path,
+            [Parameter(Position=1)]
+            [string]$Filter,
+            [Parameter()]
+            [string]$Algorithm='MD5',
+            [Parameter()]
+            [switch]$Recurse
+        )
+        Get-ChildItem -LiteralPath:$Path -Filter:$Filter -Recurse:$Recurse -File | % {
+            $Hash=(Get-FileHash -LiteralPath $_.FullName -Algorithm $Algorithm).Hash
+            if ($FileMap[$Hash]) {
+                $FileMap[$Hash]+=$_
+            } else {
+                $FileMap[$Hash]=@($_)
+            }
+        }
+    }
 }
 process {
-    $LiteralPaths=@($Path | % { [Path]::GetFullPath($_) })
-    foreach($LiteralPath in $LiteralPaths) {
-        foreach($FileFilter in $Filter) {
-            Get-ChildItem -LiteralPath:$LiteralPath -Filter:$FileFilter -Recurse:$Recurse | % {
-                $Hash=(Get-FileHash -LiteralPath $_.FullName -Algorithm $Algorithm).Hash
-                $Files=$FileMap[$Hash]
-                if ($Files) {
-                    $FileMap[$Hash]+=$_
-                } else {
-                    $FileMap[$Hash]=@($_)
-                }
+    $Path | % {
+        $LiteralPath=[Path]::GetFullPath($_)
+        if ($Filter) {
+            $Filter | % {
+                Find-Duplicate -Path $LiteralPath -Filter $_ -Recurse:$Recurse -Algorithm $Algorithm
             }
+        } else {
+            Find-Duplicate -Path $LiteralPath -Recurse:$Recurse -Algorithm $Algorithm
         }
     }
 }
@@ -33,7 +57,7 @@ end {
     $FileMap.Keys | % {
         [PSCustomObject]@{
             Hash = $_
-            Paths = ($FileMap[$_] | select -Unique)
+            Paths = ($FileMap[$_] | select -Unique | sort -Property FullName)
         }
     } | where { $_.Paths.Length -gt 1 }
 }
