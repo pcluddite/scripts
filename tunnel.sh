a379517a090dac6a9a97fba0abff26efec900c63
Tim Baxendale <pcluddite@outlook.com>
2024-02-24 22:43:56 -0500

    Add more ports to tunnel

--- tunnel.sh
+++ tunnel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-TIMS_NEWDESKTOP=192.168.0.160
-tmux new -ds tunnel ssh -L 5500:$TIMS_NEWDESKTOP:3389 -L 8888:192.168.0.1:80 minecraft.timbaxendale.com -p9022
+TIMS_NEWDESKTOP=192.168.0.15
+tmux new -ds tunnel ssh -L 5500:$TIMS_NEWDESKTOP:3389 -L 8443:192.168.0.1:443 -L 9090:192.168.0.10:9090 minecraft.timbaxendale.com -p9022
