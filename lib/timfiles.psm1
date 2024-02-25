3235ecdb2ed281de9ab25ae425bd40c595160c5c
Tim Baxendale <pcluddite@outlook.com>
2024-02-25 12:09:45 -0500

    Move Select-Text to timfiles.psm1

--- lib/timfiles.psm1
+++ lib/timfiles.psm1
@@ -23,7 +23,11 @@
  : OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  :
 #>
+using namespace System.Diagnostics.CodeAnalysis;
 using namespace System.IO
+
+$MODULE_PATH=Join-Path -Path $PSScriptRoot -ChildPath '../modules.ps1' -Resolve
+. $MODULE_PATH -Name @('string')
 
 if ($IsWindows) {
     Add-Type -AssemblyName Microsoft.VisualBasic
@@ -38,6 +42,48 @@ $REPLACE_CHARS = @{
     [char]"–" ="-"
     [char]"—" ='-'
     [char]"…" ='...'
+}
+
+function Select-Text {
+    [SuppressMessageAttribute('PSReviewUnusedParameter','IgnoreCase',Justification='false positive')]
+    param (
+        [Parameter(Mandatory,Position=0)]
+        [string]$Pattern,
+        [Parameter(Mandatory,Position=1)]
+        [string]$Path,
+        [switch]$Recurse,
+        [switch]$Regex,
+        [switch]$IgnoreCase,
+        [switch]$Plain
+    )
+    if (-not $Regex) {
+        $Pattern=(ConvertTo-Regex -Pattern $Pattern)
+        Write-Verbose "Matching regex pattern '${Pattern}'"
+    }
+    if (-not $Plain) {
+        $Highlight="$($PSStyle.Foreground.Magenta)$($PSStyle.Bold)"
+    }
+    Get-ChildItem -Path $Path -Recurse:$Recurse | % {
+        $File=$_
+        $Line=1
+        Get-Content $File | % {
+            if (($IgnoreCase -and $_ -imatch $Pattern) `
+                              -or $_ -cmatch $Pattern) {
+                [PSCustomObject]@{
+                    File = $File
+                    Line = $Line
+                    Text = $(
+                        if ($Plain) {
+                            $_
+                        } else {
+                            ($_.Split($Matches[0])) -join "${Highlight}$($Matches[0])$($PSStyle.Reset)"
+                        }
+                    )
+                }
+            }
+            $Line=$Line + 1
+        }
+    }
 }
 
 function Test-TrashPut {
